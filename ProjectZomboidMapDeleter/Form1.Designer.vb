489fd9070877ee32e343acf9a32c3c53b04c1a05
Kirschi <Kirschi94@gmx.net>
2022-08-04 21:37:26 +0200

    Add project files.

---
+++ ProjectZomboidMapDeleter/Form1.Designer.vb
@@ -0,0 +1,285 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        Me.CheckBox2 = New System.Windows.Forms.CheckBox()
+        Me.TextBox5 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.TextBox6 = New System.Windows.Forms.TextBox()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.Panel4 = New System.Windows.Forms.Panel()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.BackgroundWorker1 = New System.ComponentModel.BackgroundWorker()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(17, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(38, 15)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "From:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(17, 65)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(22, 15)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "To:"
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox1.Location = New System.Drawing.Point(15, 27)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(220, 23)
+        Me.TextBox1.TabIndex = 2
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox2.Location = New System.Drawing.Point(15, 83)
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(220, 23)
+        Me.TextBox2.TabIndex = 3
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox3.Location = New System.Drawing.Point(15, 169)
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(220, 23)
+        Me.TextBox3.TabIndex = 4
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(17, 151)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(96, 15)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Path to mapfiles:"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(17, 214)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(166, 19)
+        Me.CheckBox1.TabIndex = 6
+        Me.CheckBox1.Text = "Exclude these coordinates:"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox4.Location = New System.Drawing.Point(15, 239)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(220, 23)
+        Me.TextBox4.TabIndex = 7
+        '
+        'CheckBox2
+        '
+        Me.CheckBox2.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.CheckBox2.AutoSize = True
+        Me.CheckBox2.Checked = True
+        Me.CheckBox2.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox2.Location = New System.Drawing.Point(17, 343)
+        Me.CheckBox2.Name = "CheckBox2"
+        Me.CheckBox2.Size = New System.Drawing.Size(182, 19)
+        Me.CheckBox2.TabIndex = 8
+        Me.CheckBox2.Text = "Backup map files to this path:"
+        Me.CheckBox2.UseVisualStyleBackColor = True
+        '
+        'TextBox5
+        '
+        Me.TextBox5.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox5.Location = New System.Drawing.Point(15, 368)
+        Me.TextBox5.Name = "TextBox5"
+        Me.TextBox5.Size = New System.Drawing.Size(220, 23)
+        Me.TextBox5.TabIndex = 9
+        '
+        'Button1
+        '
+        Me.Button1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Button1.Location = New System.Drawing.Point(45, 406)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(161, 33)
+        Me.Button1.TabIndex = 10
+        Me.Button1.Text = "Go"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label4
+        '
+        Me.Label4.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(17, 268)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(22, 15)
+        Me.Label4.TabIndex = 11
+        Me.Label4.Text = "To:"
+        '
+        'TextBox6
+        '
+        Me.TextBox6.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.TextBox6.Location = New System.Drawing.Point(15, 286)
+        Me.TextBox6.Name = "TextBox6"
+        Me.TextBox6.Size = New System.Drawing.Size(220, 23)
+        Me.TextBox6.TabIndex = 12
+        '
+        'Panel1
+        '
+        Me.Panel1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Panel1.Location = New System.Drawing.Point(12, 6)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(226, 121)
+        Me.Panel1.TabIndex = 13
+        '
+        'Panel2
+        '
+        Me.Panel2.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Panel2.Location = New System.Drawing.Point(12, 134)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(226, 75)
+        Me.Panel2.TabIndex = 14
+        '
+        'Panel3
+        '
+        Me.Panel3.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Panel3.Location = New System.Drawing.Point(12, 214)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(226, 111)
+        Me.Panel3.TabIndex = 15
+        '
+        'Panel4
+        '
+        Me.Panel4.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.Panel4.Location = New System.Drawing.Point(12, 331)
+        Me.Panel4.Name = "Panel4"
+        Me.Panel4.Size = New System.Drawing.Size(226, 69)
+        Me.Panel4.TabIndex = 16
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.ProgressBar1.Location = New System.Drawing.Point(12, 445)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(226, 23)
+        Me.ProgressBar1.TabIndex = 17
+        Me.ProgressBar1.Value = 100
+        '
+        'BackgroundWorker1
+        '
+        Me.BackgroundWorker1.WorkerReportsProgress = True
+        Me.BackgroundWorker1.WorkerSupportsCancellation = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(11, 469)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(39, 15)
+        Me.Label5.TabIndex = 18
+        Me.Label5.Text = "Step 0"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(250, 487)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.TextBox6)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox5)
+        Me.Controls.Add(Me.CheckBox2)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.Panel4)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.Label5)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow
+        Me.Name = "Form1"
+        Me.Text = "PZMD"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents CheckBox2 As CheckBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents Panel4 As Panel
+    Friend WithEvents ProgressBar1 As ProgressBar
+    Friend WithEvents BackgroundWorker1 As System.ComponentModel.BackgroundWorker
+    Friend WithEvents Label5 As Label
+End Class
---
+++ ProjectZomboidMapDeleter/Form1.vb
@@ -0,0 +1,204 @@
+﻿Imports System.ComponentModel
+
+Public Class Form1
+    Dim TB1 As String = Nothing
+    Dim TB2 As String = Nothing
+    Dim TB3 As String = Nothing
+    Dim TB4 As String = Nothing
+    Dim TB5 As String = Nothing
+    Dim TB6 As String = Nothing
+    Dim CB1 As Boolean = Nothing
+    Dim CB2 As Boolean = Nothing
+
+    Dim DeletedFiles As Integer = Nothing
+    Dim Errors As Integer = Nothing
+    Dim Errorlist As String = Nothing
+
+    Dim Steps As Integer = 0
+    Dim LastPercentage As Integer = 100
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        If Button1.Text = "Go" Then
+            TB1 = TextBox1.Text
+            TB2 = TextBox2.Text
+            TB3 = TextBox3.Text
+            TB4 = TextBox4.Text
+            TB5 = TextBox5.Text
+            TB6 = TextBox6.Text
+            CB1 = CheckBox1.Checked
+            CB2 = CheckBox2.Checked
+            BackgroundWorker1.RunWorkerAsync()
+            Button1.Text = "Abort"
+        Else
+            Button1.Enabled = False
+            BackgroundWorker1.CancelAsync()
+        End If
+    End Sub
+
+    Private Function Do_Coords(TBText As String)
+        Dim TempString As String() = TBText.Split("x")
+        For Each element In TempString
+            element.Replace("x", "")
+        Next
+        TempString(0) = TempString(0).Remove(TempString(0).Length - 1, 1)
+        TempString(1) = TempString(1).Remove(TempString(1).Length - 1, 1)
+        Return TempString
+    End Function
+
+    Private Sub BackgroundWorker1_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+#Region "Exclusion"
+        Dim Counter As Integer
+        Dim Exclude As New List(Of String)
+        If CB1 Then
+            Dim FirstEx As String() = Do_Coords(TB4)
+            Dim SecondEx As String() = Do_Coords(TB6)
+
+            Dim Partm1a As Integer
+            Dim Partm1b As Integer
+
+            If FirstEx(0) < SecondEx(0) Then Partm1a = FirstEx(0) : Partm1b = SecondEx(0) _
+            Else Partm1a = SecondEx(0) : Partm1b = FirstEx(0)
+
+            'MessageBox.Show($"FirstEx: {FirstEx(0)} : {FirstEx(1)}{vbCrLf}SecondEx: {SecondEx(0)} : {SecondEx(1)}")
+            'MessageBox.Show($"Partm1a: {Partm1a}{vbCrLf}Partm1b: {Partm1b}")
+
+            'MessageBox.Show("Schwannek0")
+
+            Dim FirstExL As New List(Of String)
+            Dim StartValA As Integer = Partm1a
+            While Partm1a <= Partm1b
+                FirstExL.Add(Partm1a.ToString())
+                Partm1a += 1
+                'BackgroundWorker1.ReportProgress(100 - (100 / (Partm1b - StartValA) * (Partm1b - Partm1a)))
+            End While
+            If BackgroundWorker1.CancellationPending = True Then e.Cancel = True : Exit Sub
+            'MessageBox.Show("Schwannek1")
+            Dim Part0a As Integer
+            Dim Part0b As Integer
+            If FirstEx(1) < SecondEx(1) Then Part0a = FirstEx(1) : Part0b = SecondEx(1) _
+            Else Part0a = SecondEx(1) : Part0b = FirstEx(1)
+
+            Dim SecondExL As New List(Of String)
+            Dim StartValB As Integer = Part0a
+            While Part0a <= Part0b
+                SecondExL.Add(Part0a.ToString())
+                Part0a += 1
+                'BackgroundWorker1.ReportProgress(100 - (100 / (Part0b - StartValB) * (Part0b - Part0a)))
+            End While
+            If BackgroundWorker1.CancellationPending = True Then e.Cancel = True : Exit Sub
+            'MessageBox.Show("Schwannek2")
+            Dim AllCombA As Integer = FirstExL.Count * SecondExL.Count
+            Counter = 0
+            For Each ElementA In FirstExL
+                For Each ElementB In SecondExL
+                    Exclude.Add($"map_{ElementA}_{ElementB}.bin")
+                    Counter += 1
+                    BackgroundWorker1.ReportProgress(100 / AllCombA * Counter)
+                Next
+                If BackgroundWorker1.CancellationPending = True Then e.Cancel = True : Exit Sub
+            Next
+        End If
+#End Region
+#Region "Construction"
+        Dim DeletionList As New List(Of String)
+        Dim FirstCords As String() = Do_Coords(TB1)
+        Dim SecondCords As String() = Do_Coords(TB2)
+
+        Dim Part1a As Integer
+        Dim Part1b As Integer
+        If FirstCords(0) < SecondCords(0) Then Part1a = FirstCords(0) : Part1b = SecondCords(0) _
+            Else Part1a = SecondCords(0) : Part1b = FirstCords(0)
+        'MessageBox.Show("Schwannek3")
+        Dim FirstPart As New List(Of String)
+        Dim StartValC As Integer = Part1a
+        While Part1a <= Part1b
+            FirstPart.Add(Part1a.ToString())
+            Part1a += 1
+            'BackgroundWorker1.ReportProgress(100 - (100 / (Part1b - StartValC) * (Part1b - Part1a)))
+        End While
+        If BackgroundWorker1.CancellationPending = True Then e.Cancel = True : Exit Sub
+        'MessageBox.Show("Schwannek4")
+        Dim Part2a As Integer
+        Dim Part2b As Integer
+        If FirstCords(1) < SecondCords(1) Then Part2a = FirstCords(1) : Part2b = SecondCords(1) _
+        Else Part2a = SecondCords(1) : Part2b = FirstCords(1)
+
+        Dim SecondPart As New List(Of String)
+        Dim StartValD As Integer = Part1a
+        While Part2a <= Part2b
+            SecondPart.Add(Part2a.ToString())
+            Part2a += 1
+            'BackgroundWorker1.ReportProgress(100 - (100 / (Part2b - StartValD) * (Part2b - Part2a)))
+        End While
+        If BackgroundWorker1.CancellationPending = True Then e.Cancel = True : Exit Sub
+
+
+        Dim AllCombB As Integer = FirstPart.Count * SecondPart.Count
+        Counter = 0
+        For Each ElementA In FirstPart
+            For Each ElementB In SecondPart
+                Dim TempString = $"map_{ElementA}_{ElementB}.bin"
+                If Not Exclude.Contains(TempString) Then DeletionList.Add(TempString)
+                Counter += 1
+                BackgroundWorker1.ReportProgress(100 / AllCombB * Counter)
+            Next
+            If BackgroundWorker1.CancellationPending = True Then e.Cancel = True : Exit Sub
+        Next
+#End Region
+#Region "Deletion"
+        Dim Mappath As String = TB3
+        Dim Backuppath As String = TB5
+        If Not Mappath.EndsWith("\") Then Mappath &= "\"
+        If Not Backuppath.EndsWith("\") Then Backuppath &= "\"
+
+        If Not IO.Directory.Exists(TB3) Then MessageBox.Show("Error", "Mapfile-Path does not exist.") : Exit Sub
+        If CB2 And Not IO.Directory.Exists(TB5) Then
+            Try
+                IO.Directory.CreateDirectory(TB5)
+            Catch ex As Exception
+                MessageBox.Show("Error", "Backups-Path could not be created.")
+                Exit Sub
+            End Try
+        End If
+
+        Dim DeletedFiles_i As Integer
+        Dim Errors_i As Integer = 0
+        Dim ErrorList_i As String = ""
+
+        Counter = 0
+        For Each Element In DeletionList
+            Try
+                If CB2 Then IO.File.Copy(Mappath & Element, Backuppath & Element)
+                IO.File.Delete(Mappath & Element)
+                DeletedFiles_i += 1
+            Catch ex As Exception
+                ErrorList_i = ErrorList_i & ex.Message & vbCrLf
+                Errors_i += 1
+            End Try
+            Counter += 1
+            BackgroundWorker1.ReportProgress(100 / DeletionList.Count * Counter)
+            If BackgroundWorker1.CancellationPending = True Then e.Cancel = True : Exit Sub
+        Next
+
+        DeletedFiles = DeletedFiles_i
+        Errors = Errors_i
+        Errorlist = ErrorList_i
+#End Region
+    End Sub
+
+    Private Sub BackgroundWorker1_RunWorkerCompleted(sender As Object, e As RunWorkerCompletedEventArgs) Handles BackgroundWorker1.RunWorkerCompleted
+        MessageBox.Show($"{DeletedFiles} have been deleted, {Errors} errors have occurred.", "Process finished")
+        If Not Errorlist = Nothing And Not Errorlist = "" Then MessageBox.Show($"Errorlist:{vbCrLf}{Errorlist}")
+        If Not ProgressBar1.Value = 100 Then ProgressBar1.Value = 100
+        Steps = 0
+        Label5.Text = $"Step {Steps}"
+        LastPercentage = 100
+        Button1.Enabled = True
+        Button1.Text = "Go"
+    End Sub
+
+    Private Sub BackgroundWorker1_ProgressChanged(sender As Object, e As ProgressChangedEventArgs) Handles BackgroundWorker1.ProgressChanged
+        If LastPercentage > e.ProgressPercentage Then Steps += 1 : Label5.Text = $"Step {Steps}"
+        LastPercentage = e.ProgressPercentage
+        ProgressBar1.Value = e.ProgressPercentage
+    End Sub
+End Class
